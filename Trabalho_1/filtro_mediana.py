03237d3a9570b0391113129f74fc88264291540e
matheusces <matcesar1258@gmail.com>
2021-04-27 09:30:16 -0300

    fix in sobel filter

---
+++ Trabalho_1/filtro_mediana.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import timeit
+from utils import in_file, out_file
+
+# Aplicando filtro mediana 3x5
+def filtro_mediana(imagem):
+    w, h = imagem.size
+    nova_img = Image.new("RGB", (w, h))
+
+    for linha in range(w):
+        for coluna in range(h):
+            lista_r = []
+            lista_g = []
+            lista_b = []
+
+            for m in range(linha-1, linha+2):
+                for n in range(coluna-2, coluna+3):
+                    if m >= 0 and m < w and n >= 0 and n < h:
+                        pxl = imagem.getpixel((m,n))
+                        
+                        lista_r.append(pxl[0]) 
+                        lista_g.append(pxl[1]) 
+                        lista_b.append(pxl[2])
+                    else:
+                        # Adicionando 0 para que o número de elementos de cada lista seja 15
+                        lista_r.append(0) 
+                        lista_g.append(0) 
+                        lista_b.append(0)
+            
+            lista_r.sort()
+            lista_g.sort()
+            lista_b.sort()
+            # indice exatamente no meio = 7 
+            nova_img.putpixel((linha, coluna), (lista_r[7], lista_g[7], lista_b[7]))
+    
+    return nova_img
+            
+            
+            
+
+if __name__ == "__main__":
+    img = Image.open(in_file("Woman.jpg"))
+    
+    inicio = timeit.default_timer()
+    imagem_filto = filtro_media(img)
+    fim = timeit.default_timer()
+
+    print("Tempo de execução: {}".format(fim - inicio))
+    imagem_filto.save(out_file("WomanWithMedianaFilter.png"))
--- Trabalho_1/filtro_sobel.py
+++ Trabalho_1/filtro_sobel.py
@@ -1,104 +1,64 @@
-from P IL import Image, ImageFilter
+from PIL import Image, ImageFilter
 import timeit
 from math import sqrt
 from utils import in_file, out_file
+from filtro_media import filtro_media
 
-# aplicando filtro de sobel
-def filtrar(imagem, filtro):
+# aplicando filtro sobel horizontal e vertical
+def filtro_sobel(imagem, filtroX, filtroY):
     w, h = imagem.size
-    # nova_img = Image.new("RGB", (w, h))
-    nova_img = Image.new('L', (w, h))
+    nova_img = Image.new('RGB', (w, h))
 
     for linha in range(w):
         for coluna in range(h):
-            # soma_r = 0
-            # soma_g = 0
-            # soma_b = 0
-
-            lum = 0
+            soma_rX = 0
+            soma_gX = 0
+            soma_bX = 0
+            soma_rY = 0
+            soma_gY = 0
+            soma_bY = 0
+            # soma = 0
+            # soma_1 = 0
             i = 0
             for m in range(linha-1, linha+2):
                 for n in range(coluna-1, coluna+2):
                     if m >= 0 and m < w and n >= 0 and n < h:
                         pxl = imagem.getpixel((m,n))
-                        lum += pxl * filtro[i]
-                        # soma_r += pxl[0]*filtro[i]
-                        # soma_g += pxl[1]*filtro[i]
-                        # soma_b += pxl[2]*filtro[i]
+                        # soma += pxl * filtroX[i]
+                        # soma_1 += pxl * filtroY[i]
+                        soma_rX += pxl[0] * filtroX[i]
+                        soma_rY += pxl[0] * filtroY[i]
+                        soma_gX += pxl[1] * filtroX[i]
+                        soma_gY += pxl[1] * filtroY[i]
+                        soma_bX += pxl[2] * filtroX[i]
+                        soma_bY += pxl[2] * filtroY[i]
+
                     i+=1
 
-            nova_img.putpixel((linha, coluna), lum)
+            pxl_r = abs(soma_rX) + abs(soma_rY)
+            pxl_g = abs(soma_gX) + abs(soma_gY)
+            pxl_b = abs(soma_bX) + abs(soma_bY)
+
+            nova_img.putpixel((linha, coluna), (pxl_r, pxl_g, pxl_b))
     return nova_img
 
-# aplicando filtro sobel horizontal e vertical
-def filtro_sobel(imagem):
-    XSOBEL = ImageFilter.Kernel((3,3), 
-    [
-        -1, 0, 1,
-        -2, 0, 2,
-        -1, 0, 1
-    ], 1, 0)
-
-    YSOBEL = ImageFilter.Kernel((3,3), 
-    [
-        -1, -2, -1,
-        0, 0, 0,
-        1, 2, 1
-    ], 1, 0)
-
-
-    # XSOBEL = [-1, 0, 1,
-    #         -2, 0, 2,
-    #         -1, 0, 1]
-
-    # YSOBEL = [-1, -2, 1,
-    #         0, 0, 0,
-    #         1, 2, 1]
-
-    hsobel = imagem.filter(XSOBEL)
-    vsobel = imagem.filter(YSOBEL)
-    # hsobel = filtrar(imagem, XSOBEL)
-    # vsobel = filtrar(imagem, YSOBEL)
-
-    w, h = imagem.size
-    # imagem_filtrada = Image.new('RGB', (w, h))
-    imagem_filtrada = Image.new('L', (w, h))
-
-
-    for i in range(w):
-        for j in range(h):
-            pxl_hsobel = hsobel.getpixel((i, j))
-            pxl_vsobel = vsobel.getpixel((i, j))
-            
-            # pxl_filtrado = []
-            # for k in range(3):
-            #     if(pxl_hsobel[k] < 0):
-            #         pxl_hsobel[k] = pxl_hsobel[k] * -1
-
-            #     if(pxl_vsobel[k] < 0):
-            #         pxl_vsobel[k] = pxl_vsobel[k] * -1
-
-            #     pxl_filtrado.append(min(pxl_hsobel[k]+pxl_vsobel[k], 255))
-            if(pxl_hsobel < 0):
-                pxl_hsobel = pxl_hsobel * -1
-
-            if(pxl_vsobel < 0):
-                pxl_vsobel = pxl_vsobel * -1
-
-            valor = sqrt(pxl_vsobel**2 + pxl_hsobel**2)
-            valor = int(min(valor, 255))
-            imagem_filtrada.putpixel((i, j), valor)
-    
-    return imagem_filtrada
 
 if __name__ == "__main__":
-    # converter para escala de cinza
-    image_original = Image.open(in_file("Woman.jpg")).convert('L')
+    XSOBEL = [-1, 0, 1,
+            -2, 0, 2,
+            -1, 0, 1]
+
+    YSOBEL = [-1, -2, -1,
+                0, 0, 0,
+                1, 2, 1]
+
+    imagem_original = Image.open(in_file("Woman.jpg"))
+    imagem_original = filtro_media(imagem_original)
 
     inicio = timeit.default_timer()
-    imagem_filto = filtro_sobel(image_original)
+    imagem_filtro = filtro_sobel(imagem_original, XSOBEL, YSOBEL)
     fim = timeit.default_timer()
 
     print("Tempo de execução: {}".format(fim - inicio))
-    imagem_filto.save(out_file("WomanWithSoberFilter.png"))
+    imagem_filtro.save(out_file("WomanWithSobelFilterFinal.png"))
 
